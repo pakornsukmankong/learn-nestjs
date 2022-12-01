76240bee0b8715f8eee94d8d5f67ea8782bedb3d
pakornsukmankong <floksong.001@gmail.com>
2022-12-01 22:04:26 +0700

    first commit

---
+++ src/tag/tag.controller.ts
@@ -0,0 +1,13 @@
+import { Controller, Get } from '@nestjs/common';
+import { TagEntity } from './tag.entity';
+import { TagService } from './tag.service';
+
+@Controller('tag')
+export class TagController {
+  constructor(private readonly tagService: TagService) {}
+
+  @Get()
+  async findAll(): Promise<TagEntity[]> {
+    return await this.tagService.findAll();
+  }
+}
