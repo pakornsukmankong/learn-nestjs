401bfff0f2854757d65ee4852e205d7c31927fe6
Pakorn Sukmankong <pakorn.su@Pakorns-MacBook-Pro.local>
2022-12-09 17:28:55 +0700

    feat: profile

---
+++ src/profile/profile.controller.ts
@@ -0,0 +1,55 @@
+import { User } from '@app/user/decorators/user.decorator';
+import { AuthGuard } from '@app/user/guards/auth.guard';
+import {
+  Controller,
+  Delete,
+  Get,
+  Param,
+  Post,
+  UseGuards,
+} from '@nestjs/common';
+import { ProfileService } from './profile.service';
+import { ProfileResponseInterface } from './types/profileResponse.interface';
+
+@Controller('profiles')
+export class ProfileController {
+  constructor(private readonly profileService: ProfileService) {}
+
+  @Get(':username')
+  async getProfile(
+    @User('id') currentUserId: number,
+    @Param('username') username: string,
+  ): Promise<ProfileResponseInterface> {
+    const profile = await this.profileService.getProfile(
+      currentUserId,
+      username,
+    );
+    return this.profileService.buildProfileResponse(profile);
+  }
+
+  @Post(':username/follow')
+  @UseGuards(AuthGuard)
+  async followProfile(
+    @User('id') currentUserID: number,
+    @Param('username') profileUsername: string,
+  ): Promise<ProfileResponseInterface> {
+    const profile = await this.profileService.followProfile(
+      currentUserID,
+      profileUsername,
+    );
+    return this.profileService.buildProfileResponse(profile);
+  }
+
+  @Delete(':username/follow')
+  @UseGuards(AuthGuard)
+  async unfollowProfile(
+    @User('id') currentUserID: number,
+    @Param('username') profileUsername: string,
+  ): Promise<ProfileResponseInterface> {
+    const profile = await this.profileService.unfollowProfile(
+      currentUserID,
+      profileUsername,
+    );
+    return this.profileService.buildProfileResponse(profile);
+  }
+}
---
+++ src/profile/profile.service.ts
@@ -0,0 +1,99 @@
+import { UserEntity } from '@app/user/user.entity';
+import {
+  BadRequestException,
+  Injectable,
+  NotFoundException,
+} from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { FollowEntity } from './profile.entity';
+import { ProfileType } from './types/profile.type';
+import { ProfileResponseInterface } from './types/profileResponse.interface';
+
+@Injectable()
+export class ProfileService {
+  constructor(
+    @InjectRepository(UserEntity)
+    private readonly userRepository: Repository<UserEntity>,
+    @InjectRepository(FollowEntity)
+    private readonly followRepository: Repository<FollowEntity>,
+  ) {}
+
+  async getProfile(
+    currentUserId: number,
+    profileUsername: string,
+  ): Promise<ProfileType> {
+    const user = await this.userRepository.findOne({
+      where: { username: profileUsername },
+    });
+
+    if (!user) {
+      throw new NotFoundException('Profile does not exist');
+    }
+
+    const follow = await this.followRepository.findOne({
+      where: { followerId: currentUserId, followingId: user.id },
+    });
+
+    delete user.email;
+    return { ...user, following: Boolean(follow) };
+  }
+
+  async followProfile(
+    currentUserId: number,
+    profileUsername: string,
+  ): Promise<ProfileType> {
+    const user = await this.userRepository.findOne({
+      where: { username: profileUsername },
+    });
+
+    if (!user) {
+      throw new NotFoundException('Profile does not exist');
+    }
+
+    if (currentUserId === user.id) {
+      throw new BadRequestException('Cannot follow yourself');
+    }
+
+    const follow = await this.followRepository.findOne({
+      where: { followerId: currentUserId, followingId: user.id },
+    });
+
+    if (!follow) {
+      const followToCreate = new FollowEntity();
+      followToCreate.followerId = currentUserId;
+      followToCreate.followingId = user.id;
+      await this.followRepository.save(followToCreate);
+    }
+
+    return { ...user, following: true };
+  }
+
+  async unfollowProfile(
+    currentUserId: number,
+    profileUsername: string,
+  ): Promise<ProfileType> {
+    const user = await this.userRepository.findOne({
+      where: { username: profileUsername },
+    });
+
+    if (!user) {
+      throw new NotFoundException('Profile does not exist');
+    }
+
+    if (currentUserId === user.id) {
+      throw new BadRequestException('Cannot unfollow yourself');
+    }
+
+    await this.followRepository.delete({
+      followerId: currentUserId,
+      followingId: user.id,
+    });
+
+    return { ...user, following: false };
+  }
+
+  buildProfileResponse(profile: ProfileType): ProfileResponseInterface {
+    return { profile };
+  }
+}
